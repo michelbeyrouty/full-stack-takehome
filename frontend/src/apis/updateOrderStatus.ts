b6142f5e64a2b344d672766785bd0b6977570cd3
Michel Beyrouty <michelbeyrouty@gmail.com>
2023-07-04 10:03:42 -0400

    Add missing env.example (#28)
    * Feature/3 Add request info logging (#4)
    
    * [3] Add request/response logger
    
    * [3] Fix logger
    
    * [3] Add documentation
    
    * [3] Add documentation
    
    * Feature/7 Add list orders api 🎮 (#5)
    
    * [7] Add list orders api
    
    * [7] Cleanup code
    
    * Feature/15 Add backend tests 🧪 (#7)
    
    * [15] Add Jest
    
    * [15] Setup test structure
    
    * [15] Setup test structure
    
    * Feature/9 Add get order api 🎮 (#8)
    
    * [9] Cleanup
    
    * [9] Add getOrder
    
    * [9] Add tests
    
    * [9] Cleanup
    
    * [9] Cleanup
    
    * [9] Cleanup
    
    * [9] Cleanup
    
    * [9] Cleanup
    
    * Feature/14 Create card component 🃏 (#9)
    
    * [14] Cleanup
    
    * [14] Cleanup
    
    * [14] Cleanup
    
    * [14] Add Card
    
    * Feature/5 Add app layout (#10)
    
    * [14] Add Layout Component
    
    * [5] Cleanup
    
    * [5] Cleanup
    
    * [5] Cleanup
    
    * Feature/4 Add order card 🃏 (#11)
    
    * [4] Cleanup
    
    * [4] Cleanup
    
    * [4] Cleanup
    
    * [4] Cleanup
    
    * [4] Cleanup
    
    * [4] Cleanup
    
    * [4] Cleanup
    
    * [4] Cleanup
    
    * Feature/6 Finalize orders page 💰 (#12)
    
    * [6] Finalize orders page
    
    * [6] Cleanup
    
    * [6] Cleanup
    
    * [6] Cleanup
    
    * [6] Cleanup
    
    * [6] Cleanup
    
    * Feature/8 Add order details page ⚜️ (#13)
    
    * [8] Add missing pages
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * [8] Add Order Details page
    
    * Feature/10 Add order from and Cleanup ☢️ (#14)
    
    * [10] Add Submit Order Page
    
    * [10] Add Submit Order Page
    
    * [10] Add Submit Order Page
    
    * [10] Add Submit Order Page
    
    * [10] Clean up
    
    * [10] Clean up
    
    * [10] Clean up
    
    * [10] Clean up
    
    * [10] Clean up
    
    * [10] Clean up
    
    * [10] Clean up Button
    
    * [10] Clean up Input
    
    * [10] Clean up Input
    
    * [10] Clean up Input
    
    * [10] Clean up Loader
    
    * [10] Clean up OrderCard
    
    * [10] Clean up OrderForm
    
    * [10] Clean up Orders
    
    * [10] Clean up Orders
    
    * [10] Add interface
    
    * [10] Add interface
    
    * Feature/12 Add productivity page 🎧 (#15)
    
    * [12] Add productivity page
    
    * [12] Add productivity page
    
    * [12] Add productivity page
    
    * [12] Add productivity page
    
    * [12] Add productivity page
    
    * [12] Add productivity page
    
    * [12] Add productivity page
    
    * Feature/11 Add create work order API 🎮 (#16)
    
    * [11] Add create order api
    
    * [11] Add create order api
    
    * [11] Add create order api
    
    * [11] Fix interfaces
    
    * [11] Fix interfaces
    
    * [11] Remove uneeded route
    
    * Feature/13 Add List unproductive users API 🎮 (#17)
    
    * [13] Add list inactive users
    
    * [13] Cleanup
    
    * Feature/17 Add update order status API 🎮 (#18)
    
    * [17] Add Update Order Status API
    
    * [17] Add OrderForm
    
    * [17] Fix cors
    
    * Feature/18 Add API integrations 🦾 (#19)
    
    * [18] Update documentation
    
    * [18] Add integration
    
    * [18] Add integration
    
    * [18] Add integration
    
    * [18] Add integration
    
    * [18] Add integration
    
    * [18] Add postman collection
    
    * [18] Add modal
    
    * [18] Add modal
    
    * [18] Add modal
    
    * Feature/20 Final cleanups 🪩 (#20)
    
    * [20] Cleanup
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Cleanup tests
    
    * [20] Add env (#23)
    
    * [21] Add docker file for backend (#26)
    
    * [hotfix] Add env example (#27)

--- frontend/src/apis/updateOrderStatus.ts
+++ frontend/src/apis/updateOrderStatus.ts
@@ -1,9 +1,9 @@
 export default async function updateOrderStatus(orderId: number) {
     try {
-
         const requestOptions = {
             method: 'PUT',
         };
+
 
         await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/orders/${orderId}/status`, requestOptions);
     } catch (e) {
